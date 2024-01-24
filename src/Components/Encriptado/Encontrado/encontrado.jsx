db7ff56fd01b06b22eed2613dc0db80ab363df07
Gonza <gonzavillalba123@gmail.com>
2024-01-24 14:57:44 -0300

    Haciendolo responsive

--- src/Components/Encriptado/Encontrado/encontrado.jsx
+++ src/Components/Encriptado/Encontrado/encontrado.jsx
@@ -5,11 +5,12 @@ import { FaCopy } from "react-icons/fa";
 
 const Encontrado = ({encriptado}) => {
 
-    const [copy, setCopy] = useState(false);
+    const [copy, setCopy] = useState(true);
     const textareaRef = useRef();
 
     return(
         <div className="encontrado">
+            {copy === false ? <FaCopy className="blank"></FaCopy> : <FaCopy className='copy'/>}
             <textarea 
             ref = {textareaRef}
             readOnly 
@@ -21,7 +22,6 @@ const Encontrado = ({encriptado}) => {
                     setTimeout(()=> {setCopy(false)}, 2000);
                 }}>
                     Copiar</button>
-            {copy === false ? <></> : <FaCopy className='copy'/>}
         </div>
     )
 }
